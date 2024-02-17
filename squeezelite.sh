242375cd5ef762aa404986d5e68a629f21e68b50
aschamberger <mail@andreass.net>
2024-02-17 14:50:59 +0100

    Add alsa volume control option

--- squeezelite.sh
+++ squeezelite.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 OUTPUT_DEVICE="${OUTPUT_DEVICE:-default}"
+MIXER_DEVICE="${MIXER_DEVICE:-$OUTPUT_DEVICE}"
 MAC_ADDRESS="${MAC_ADDRESS:-02:00:00:00:00:00}"
 
 POWER_SCRIPT=""
@@ -8,5 +9,10 @@ if [[ -n "$GPIO_PSU_RELAY" || -n "$GPIO_MUTE" || -n "$GPIO_SHUTDOWN" || -n "$GPI
     POWER_SCRIPT=" -S /usr/local/bin/power_mute.sh"
 fi
 
+ALSA_VOLUME_CONTROL=""
+if [[ -n "$VOLUME_CONTROL" ]]; then
+    ALSA_VOLUME_CONTROL=" -O ${MIXER_DEVICE} -V ${VOLUME_CONTROL}"
+fi
+
 # run squeezelite with user squeezelite
-exec su-exec squeezelite squeezelite -a 80:::0: -N /config/squeeze.name -o $OUTPUT_DEVICE -m $MAC_ADDRESS$POWER_SCRIPT
+exec su-exec squeezelite squeezelite -a 80:::0: -N /config/squeeze.name -o $OUTPUT_DEVICE$ALSA_VOLUME_CONTROL -m $MAC_ADDRESS$POWER_SCRIPT
