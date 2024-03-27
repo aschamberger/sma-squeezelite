7ad7445daf36e61ba1eb3524f674f67d9e506ff3
aschamberger <mail@andreass.net>
2024-03-27 21:25:46 +0100

    fix permissions for line in script

--- squeezelite.sh
+++ squeezelite.sh
@@ -19,5 +19,8 @@ if [[ -n "$VOLUME_CONTROL" ]]; then
     ALSA_VOLUME_CONTROL=" -O ${MIXER_DEVICE} -V ${VOLUME_CONTROL}"
 fi
 
+# https://github.com/moby/moby/issues/31243#issuecomment-406879017
+chmod o+w /dev/stdout
+
 # run squeezelite with user squeezelite
 exec su-exec squeezelite squeezelite -a 80:::0: -N /config/squeeze.name -o $OUTPUT_DEVICE$ALSA_VOLUME_CONTROL -m $MAC_ADDRESS$POWER_SCRIPT$LINE_IN_SCRIPT
