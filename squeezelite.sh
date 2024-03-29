797b381523eebe849f4eb7a31d08260c5b3480f1
aschamberger <mail@andreass.net>
2024-03-29 07:22:06 +0100

    Add logging option via env var

--- squeezelite.sh
+++ squeezelite.sh
@@ -4,6 +4,11 @@ OUTPUT_DEVICE="${OUTPUT_DEVICE:-default}"
 MIXER_DEVICE="${MIXER_DEVICE:-$OUTPUT_DEVICE}"
 MAC_ADDRESS="${MAC_ADDRESS:-02:00:00:00:00:00}"
 
+ALSA_VOLUME_CONTROL=""
+if [[ -n "$VOLUME_CONTROL" ]]; then
+    ALSA_VOLUME_CONTROL=" -O ${MIXER_DEVICE} -V ${VOLUME_CONTROL}"
+fi
+
 POWER_SCRIPT=""
 if [[ -n "$GPIO_PSU_RELAY" || -n "$GPIO_MUTE" || -n "$GPIO_SHUTDOWN" || -n "$GPIO_SPS" || -n "$HASS_SWITCH" ]]; then
     POWER_SCRIPT=" -S /usr/local/bin/power_mute.sh"
@@ -14,13 +19,13 @@ if [[ -n "$INPUT_DEVICE" ]]; then
     LINE_IN_SCRIPT=" -T /usr/local/bin/line_in.sh"
 fi
 
-ALSA_VOLUME_CONTROL=""
-if [[ -n "$VOLUME_CONTROL" ]]; then
-    ALSA_VOLUME_CONTROL=" -O ${MIXER_DEVICE} -V ${VOLUME_CONTROL}"
+ADD_LOGGING=""
+if [[ -n "$SQUEEZELITE_LOGGING" ]]; then
+    ADD_LOGGING=" -d ${SQUEEZELITE_LOGGING}"
 fi
 
 # https://github.com/moby/moby/issues/31243#issuecomment-406879017
 chmod o+w /dev/stdout
 
 # run squeezelite with user squeezelite
-exec su-exec squeezelite squeezelite -a 80:::0: -N /config/squeeze.name -o $OUTPUT_DEVICE$ALSA_VOLUME_CONTROL -m $MAC_ADDRESS$POWER_SCRIPT$LINE_IN_SCRIPT
+exec su-exec squeezelite squeezelite -a 80:::0: -N /config/squeeze.name -o $OUTPUT_DEVICE$ALSA_VOLUME_CONTROL -m $MAC_ADDRESS$POWER_SCRIPT$LINE_IN_SCRIPT$ADD_LOGGING
